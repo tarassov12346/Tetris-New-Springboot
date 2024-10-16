692fb52a200d8e1728fee57aa18194118ca00ab4
Andrei Tarassov <tamogenia@gmail.com>
2024-10-16 19:43:08 +0300

    makeDeskTopSnapShot thru PLaywright

--- src/main/java/com/app/game/tetris/daoserviceImpl/DaoMongo.java
+++ src/main/java/com/app/game/tetris/daoserviceImpl/DaoMongo.java
@@ -22,10 +22,9 @@ import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,18 +38,18 @@ public class DaoMongo implements DaoMongoService {
     @Value("${mongodbPath}")
     String mongodbPath;
 
-    String pathToShots=System.getProperty("user.dir") + "\\src\\main\\webapp\\shots\\";
-    String pathToImageMongoPreparedShots= System.getProperty("user.dir") + "\\src\\main\\webapp\\mongoPrepareShots\\";
-    String uri="mongodb://localhost";
+    String pathToShots = System.getProperty("user.dir") + "\\src\\main\\webapp\\shots\\";
+    String pathToImageMongoPreparedShots = System.getProperty("user.dir") + "\\src\\main\\webapp\\mongoPrepareShots\\";
+    String uri = "mongodb://localhost";
 
-  //  String pathToShots=System.getProperty("user.dir") + "/src/main/webapp/shots/";
-  //  String pathToImageMongoPreparedShots= System.getProperty("user.dir") + "/src/main/webapp/mongoPrepareShots/";
-  //  String uri="mongodb://springboot-mongo";
+    //  String pathToShots=System.getProperty("user.dir") + "/src/main/webapp/shots/";
+    //  String pathToImageMongoPreparedShots= System.getProperty("user.dir") + "/src/main/webapp/mongoPrepareShots/";
+    //  String uri="mongodb://springboot-mongo";
 
     @Override
     public void runMongoServer() {
         try {
-            Runtime.getRuntime().exec(new String [] {mongodbPath});
+            Runtime.getRuntime().exec(new String[]{mongodbPath});
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -65,10 +64,10 @@ public class DaoMongo implements DaoMongoService {
         list.add("Bonny");
         list.add("Ira");
         list.add("Wolfy");
-        list.forEach(playerName-> {
+        list.forEach(playerName -> {
             fillMongoDB(playerName);
-            fillMongoDB(playerName+"deskTopSnapShotBest");
-            fillMongoDB(playerName+"deskTopSnapShot");
+            fillMongoDB(playerName + "deskTopSnapShotBest");
+            fillMongoDB(playerName + "deskTopSnapShot");
         });
     }
 
@@ -84,7 +83,7 @@ public class DaoMongo implements DaoMongoService {
         MongoClient mongoClient = MongoClients.create(uri);
         MongoDatabase database = mongoClient.getDatabase("shopDB");
         GridFSBucket gridFSBucket = GridFSBuckets.create(database);
-        GridFSFindIterable gridFSFile = gridFSBucket.find(Filters.eq("filename", playerName + fileName+".jpg"));
+        GridFSFindIterable gridFSFile = gridFSBucket.find(Filters.eq("filename", playerName + fileName + ".jpg"));
         while (gridFSFile.cursor().hasNext()) {
             gridFSBucket.delete(gridFSFile.cursor().next().getId());
         }
@@ -98,14 +97,14 @@ public class DaoMongo implements DaoMongoService {
         GridFSBucket gridFSBucket = GridFSBuckets.create(database);
         byte[] data = new byte[0];
         try {
-            data = Files.readAllBytes(Path.of(pathToShots+fileName+".jpg"));
+            data = Files.readAllBytes(Path.of(pathToShots + fileName + ".jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         GridFSUploadOptions options = new GridFSUploadOptions()
                 .chunkSizeBytes(1048576)
                 .metadata(new Document("type", "jpg"));
-        try (GridFSUploadStream uploadStream = gridFSBucket.openUploadStream(playerName + fileName+".jpg", options)) {
+        try (GridFSUploadStream uploadStream = gridFSBucket.openUploadStream(playerName + fileName + ".jpg", options)) {
             // Writes file data to the GridFS upload stream
             uploadStream.write(data);
             uploadStream.flush();
@@ -134,26 +133,7 @@ public class DaoMongo implements DaoMongoService {
 
     @Override
     public void makeDesktopSnapshot(String fileNameDetail) {
-       Robot robot = null;
-        System.setProperty("java.awt.headless", "false");
-        try {
-            robot = new Robot();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
         String format = "jpg";
-        String fileName = pathToShots + fileNameDetail + "." + format;
-        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-        try {
-            ImageIO.write(screenFullImage, format, new File(fileName));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    //    try (Playwright playwright = Playwright.create()){
-    //        playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
-    //    }
-    /*      String format = "jpg";
         String fileName = pathToShots + fileNameDetail + "." + format;
 
         try (Playwright playwright = Playwright.create()) {
@@ -162,8 +142,7 @@ public class DaoMongo implements DaoMongoService {
             page.navigate("http://localhost:8080/6");
             page.waitForURL("http://localhost:8080/6");
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(fileName)));
-        }*/
-
+        }
     }
 
     @Override
@@ -176,7 +155,7 @@ public class DaoMongo implements DaoMongoService {
 // Downloads a file to an output stream
         try (ByteArrayOutputStream buffer = new ByteArrayOutputStream();) {
             if (fileName.equals("mugShot")) gridFSBucket.downloadToStream(playerName + ".jpg", buffer, downloadOptions);
-            else gridFSBucket.downloadToStream(playerName + fileName+".jpg", buffer, downloadOptions);
+            else gridFSBucket.downloadToStream(playerName + fileName + ".jpg", buffer, downloadOptions);
             imagenEnBytes = buffer.toByteArray();
             buffer.flush();
         } catch (FileNotFoundException e) {
@@ -188,20 +167,20 @@ public class DaoMongo implements DaoMongoService {
         return imagenEnBytes;
     }
 
-    private void fillMongoDB(String fileName){
+    private void fillMongoDB(String fileName) {
         MongoClient mongoClient = MongoClients.create(uri);
         MongoDatabase database = mongoClient.getDatabase("shopDB");
         GridFSBucket gridFSBucket = GridFSBuckets.create(database);
         byte[] data = new byte[0];
         try {
-            data = Files.readAllBytes(Path.of(pathToImageMongoPreparedShots+fileName+".jpg"));
+            data = Files.readAllBytes(Path.of(pathToImageMongoPreparedShots + fileName + ".jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         GridFSUploadOptions options = new GridFSUploadOptions()
                 .chunkSizeBytes(1048576)
                 .metadata(new Document("type", "jpg"));
-        try (GridFSUploadStream uploadStream = gridFSBucket.openUploadStream(fileName+".jpg", options)) {
+        try (GridFSUploadStream uploadStream = gridFSBucket.openUploadStream(fileName + ".jpg", options)) {
             // Writes file data to the GridFS upload stream
             uploadStream.write(data);
             uploadStream.flush();
